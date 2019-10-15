e481ae7f35efc4d295b59ec85cf8a0c9404f5143
Jake Day <jake@Jakes-MacBook-Pro.local>
2019-10-15 14:08:52 -0700

    Updates

---
+++ precache-manifest.331b0df59c0ce8677f91aa712856c0d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a94a3e9fc4e61b67b88c65fc64198c",
+    "url": "/React-Rolodex/index.html"
+  },
+  {
+    "revision": "309010411394bb6a68c0",
+    "url": "/React-Rolodex/static/css/main.9cb6031b.chunk.css"
+  },
+  {
+    "revision": "157cdb102858af4505a9",
+    "url": "/React-Rolodex/static/js/2.6da1f240.chunk.js"
+  },
+  {
+    "revision": "309010411394bb6a68c0",
+    "url": "/React-Rolodex/static/js/main.38e09eb3.chunk.js"
+  },
+  {
+    "revision": "f03d5e964812665062b0",
+    "url": "/React-Rolodex/static/js/runtime-main.aff55d44.js"
+  }
+]);
